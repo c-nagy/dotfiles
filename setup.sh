a05a00ae17c2bc7831dff4e2d49075fa882ffc24
Craig Nagy <craig.nagy131@gmail.com>
2019-08-07 23:32:03 -0600

    tmux stylin

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
 apt update && apt upgrade -y
-apt install tmux zsh
+apt install tmux zsh htop
 chsh -s /usr/bin/zsh root
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 cp .zshrc ~/.zshrc
 cp .tmux.conf ~/.tmux.conf
+echo "sudo -i" >> /home/ec2-user/.bashrc
+source ~/.zshrc
