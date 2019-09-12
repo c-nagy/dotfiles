e4dc19f5456213501c7cb034d2c5b285c6397300
Craig Nagy <craig.nagy131@gmail.com>
2019-09-11 21:38:27 -0600

    small fixes

--- .vimrc
+++ .vimrc
@@ -3,3 +3,6 @@ set mouse=
 
 " Ignore case in search
 set ignorecase
+
+" Enable syntax highlighting
+syntax on
--- setup.sh
+++ setup.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 
-apt update && apt upgrade -y
+apt update
 apt install htop tmux vim zsh
 chsh -s /usr/bin/zsh root
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 cp .zshrc ~/.zshrc
 cp .tmux.conf ~/.tmux.conf
 cp .vimrc ~/.vimrc
-echo "sudo -i" >> /home/ec2-user/.bashrc
 zsh
