d6268066c611bc221bace802073136843c4b6478
Craig Nagy <craig.nagy131@gmail.com>
2019-10-18 13:11:30 -0600

    adding non-interactive apt upgrade to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,23 @@
 #!/usr/bin/env bash
 
+# Update with non-interactive apt
 apt update
+apt remove apt-listchanges -y
+DEBIAN_FRONTEND=noninteractive apt upgrade -y
+
+# Install packages
 apt install -y htop tmux vim zsh
+
+# Change shell to Zsh
 chsh -s $(which zsh) root
+
+# Set up Tmux plugin manager (comes with tmux sensible plugin)
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+# Copy config files into home dir
 cp .zshrc ~/.zshrc
 cp .tmux.conf ~/.tmux.conf
 cp .vimrc ~/.vimrc
+
+# Start working
 zsh
