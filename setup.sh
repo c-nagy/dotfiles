06b862b95b8110de51ffd623b52a8fa33a23a6c9
@cnagy <3061769+c-nagy@users.noreply.github.com>
2020-06-17 14:51:05 -0600

    Adding 'la' alias to ls -al

--- setup.sh
+++ setup.sh
@@ -22,7 +22,7 @@ git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 # Grab and install latest release version of LSD (ls replacement). Hacky download command but seems to work well:
 curl -s -L --output lsd.deb `curl -L https://github.com/Peltoche/lsd/releases/latest -s | grep -E 'lsd_.*_amd64.deb' | sort -u | grep href | cut -d '"' -f2 | sed 's/^/https:\/\/github.com/g'`
-dpkg -i lsd.deb && rm lsd.deb && echo 'alias ls="lsd"' >> ~/.zshrc && echo 'alias lt="lsd -l --tree"' >> ~/.zshrc
+dpkg -i lsd.deb && rm lsd.deb && echo 'alias ls="lsd"' >> ~/.zshrc && echo 'alias la="lsd -al"' >> ~/.zshrc && echo 'alias lt="lsd -l --tree"' >> ~/.zshrc
 
 # Download and install patched font that allows for more icons for LSD
 curl -s -L --output /usr/share/fonts/ https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/Hack/Bold/complete/Hack%20Bold%20Nerd%20Font%20Complete.ttf
