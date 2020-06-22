077cc2a57fc75427d8325cc78c4617cf702a011b
@cnagy <3061769+c-nagy@users.noreply.github.com>
2020-06-22 13:16:55 -0600

    Changed patched font install (for LSD icons) to an apt command instead

--- setup.sh
+++ setup.sh
@@ -28,7 +28,7 @@ sudo apt remove apt-listchanges -y
 DEBIAN_FRONTEND=noninteractive sudo apt upgrade -y
 
 # Install packages
-sudo apt install -y tmux htop vim zsh dnsutils
+sudo apt install -y tmux htop vim zsh dnsutils fonts-hack-ttf
 
 # Change shell to Zsh
 chsh -s $(which zsh) $(whoami)
@@ -40,10 +40,6 @@ git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 # Grab and install latest release version of LSD (ls replacement). Hacky download command but seems to work well:
 curl -s -L --output lsd.deb `curl -L https://github.com/Peltoche/lsd/releases/latest -s | grep -E 'lsd_.*_amd64.deb' | sort -u | grep href | cut -d '"' -f2 | sed 's/^/https:\/\/github.com/g'`
 dpkg -i lsd.deb && rm lsd.deb && echo 'alias ls="lsd"' >> ~/.zshrc && echo 'alias la="lsd -al"' >> ~/.zshrc && echo 'alias lt="lsd -l --tree"' >> ~/.zshrc
-
-# Download and install patched font that allows for more icons for LSD
-curl -s -L --output /usr/share/fonts/ https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/Hack/Bold/complete/Hack%20Bold%20Nerd%20Font%20Complete.ttf
-fc-cache -f -v
 
 # Install bashtop (provides more info than htop)
 git clone https://github.com/aristocratos/bashtop.git /opt/bashtop/
