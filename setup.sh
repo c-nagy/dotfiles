3be6106cc5612fd875b65a8c66b536d2a8db83f5
@cnagy <3061769+c-nagy@users.noreply.github.com>
2020-07-02 18:43:44 -0600

    Making pwndrop service install optional

--- setup.sh
+++ setup.sh
@@ -5,13 +5,15 @@ cp zshrc.zsh ~/.zshrc
 cp tmux.conf ~/.tmux.conf
 cp vimrc.vim ~/.vimrc
 
-# Interactively get hostname and project name
+# Interactively get name of host
 echo "[?] What hostname should this box have? Needs to be DNS compliant, so no spaces/most special chars and keep it short. Example: ClientEXT"
 read -p 'Enter hostname: ' hostname
 if [[ -z "$hostname" ]]; then
    printf '%s\n' "No hostname appears to have been entered, quitting."
    exit 1
 fi
+
+# Interactively get project name
 echo "[?] What should this project be called? This is used for naming the primary Tmux session. Keep it short, but spaces are okay. Example: Client External 2020"
 read -p 'Enter project name: ' projectname
 if [[ -z "$projectname" ]]; then
@@ -21,6 +23,16 @@ fi
 hostnamectl set-hostname $hostname
 echo "127.0.0.1 $hostname" >> /etc/hosts
 sed -i "s/base/$projectname/" ~/.zshrc
+
+# Check if we should install pwndrop service
+echo "[?] Enter y if you want to install pwndrop now (it uses ports 80 and 443)."
+read -p 'Enter y if you want pwndrop installed now: ' pwndropChoice
+if [[ "$pwndropChoice" == "y" ]]; then
+   printf '%s\n' "You said y :) Trying to install pwndrop now..."
+   curl https://raw.githubusercontent.com/kgretzky/pwndrop/master/install_linux.sh | sudo bash
+else
+   printf '%s\n' "You didn't say y, that's okay. Not installing pwndrop."
+fi
 
 # Upgrade OS with non-interactive apt
 sudo apt update
@@ -51,10 +63,6 @@ git clone https://github.com/zsh-users/zsh-autosuggestions.git /opt/zsh-autosugg
 
 # Disable system beeps
 rmmod pcspkr && echo "blacklist pcspkr" >> /etc/modprobe.d/blacklist.conf
-
-# Add pwndrop service
-systemctl stop apache2
-curl https://raw.githubusercontent.com/kgretzky/pwndrop/master/install_linux.sh | sudo bash
 
 # Reminder to refresh env and reboot
 echo "Recommend to reboot now and, once back up, press tmux prefix+I to ensure tmux plugins are initialized."
