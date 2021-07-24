824b726258a6dfdbb675a1f4d266be2202aac646
@cnagy <3061769+c-nagy@users.noreply.github.com>
2021-07-24 08:12:01 +0000

    Little bit of cleanup

--- setup.sh
+++ setup.sh
@@ -1,90 +1,55 @@
 #!/usr/bin/env bash
 
-# Todo: Refactor script so it's effecient at running as an update script for everything too. Maybe just add an --update flag, but it would be cool to have it autodetect if it should be a 'update' or 'fresh' install and only display pertinent prompts.
-
 # Copy/overwrite config files in home directory
 cp zshrc.zsh ~/.zshrc
 cp tmux.conf ~/.tmux.conf
 cp vimrc.vim ~/.vimrc
 
-# Install packages
-sudo apt update
-sudo apt full-upgrade
-sudo apt install -y curl fortune tmux htop vim zsh dnsutils fonts-hack-ttf ncat xclip zsh-autosuggestions zsh-syntax-highlighting
+# Apt upgrades and cleanups
+green=$(tput setaf 2)
+yellow=$(tput setaf 3)
+reset=$(tput sgr0)
 
-# Todo: Make this block optional with a prompt. Ensure the hostname changing steps are in here rather than spread out in this script. 
-# Interactively get name of host
-echo "[?] What hostname should this box have? Needs to be DNS compliant, so no spaces/most special chars and keep it short. Example: ClientEXT"
-read -p 'Enter hostname: ' hostname
-if [[ -z "$hostname" ]]; then
-   printf '%s\n' "No hostname appears to have been entered, quitting."
-   exit 1
-fi
+echo "${yellow}==>${reset} apt update..."
+sudo apt update 2>&1
+echo "${green}==>${reset} apt update finished."
 
-# Interactively get project name
-echo "[?] What should this project be called? This is used for naming the primary Tmux session. Keep it short, but spaces are okay. Example: Client External 2020"
-read -p 'Enter project name: ' projectname
-if [[ -z "$projectname" ]]; then
-   printf '%s\n' "No project name appears to have been entered, quitting."
-   exit 1
-fi
-hostnamectl set-hostname $hostname
-echo "127.0.0.1 $hostname" >> /etc/hosts
-sed -i "s/base/$projectname/" ~/.zshrc
+echo "${yellow}==>${reset} Running full-upgrade..."
+sudo apt full-upgrade -y 2>&1
+echo "${green}==>${reset} Finished full-upgrade"
 
-# Check if we should install pwndrop service
-echo "[?] Enter y if you want to install pwndrop now (it uses ports 80 and 443 by default)."
-read -p 'Enter y if you want pwndrop installed now: ' pwndropChoice
-shopt -s nocasematch # Case insensitive for pwndropChoice input
-if [[ "$pwndropChoice" == "y" ]]; then
-   printf '%s\n' "You said y :) Trying to install pwndrop now..."
-   curl https://raw.githubusercontent.com/kgretzky/pwndrop/master/install_linux.sh | sudo bash
-else
-   printf '%s\n' "You didn't say y, that's okay. Not installing pwndrop."
-fi
+echo "${green}==>${reset} Cleaning..."
+sudo apt autoclean -y 2>&1
+sudo apt autoremove -y 2>&1
+echo "${green}==>${reset} All Updates & Cleanups Finished"
 
-# Check if we should append sudo -i to the .bashrc files of everybody
-echo "[?] Enter y if you want to append sudo -i to all .bashrc files in /home/ for auto-elevation to root."
-read -p 'Enter y if you want sudo -i appended to all .bashrc files: ' bashrcChoice
-shopt -s nocasematch # Case insensitive for bashrcChoice input
-if [[ "$bashrcChoice" == "y" ]]; then
-   printf '%s\n' "You said y :) Appending sudo -i to the .bashrc files now..."
-   find /home/ -type f -name '.bashrc' -exec sh -c "echo sudo -i >> {}" \;
-else
-   printf '%s\n' "You didn't say y, that's okay. Not appending sudo -i."
-fi
+# Install packages available in Apt
+sudo apt install -y curl fortune tmux htop vim zsh dnsutils fonts-hack-ttf ncat xclip zsh-autosuggestions zsh-syntax-highlighting lolcat
 
-# Check if we should upgrade OS with apt
-# Refactor this so that 'apt update' doesn't have to be ran multiple times in this script.
-echo "[?] Enter y if you want to have apt update and upgrade ran now."
-read -p 'Enter y to start the upgrade: ' upgradeChoice
-shopt -s nocasematch # Case insensitive for upgradeChoice input
-if [[ "$upgradeChoice" == "y" ]]; then
-   printf '%s\n' "You said y :) Upgrading via apt now..."
-   sudo apt remove apt-listchanges -y
-   DEBIAN_FRONTEND=noninteractive sudo apt upgrade -y
-else
-   printf '%s\n' "You didn't say y, that's okay. Not updating OS."
-fi
+# Update Joplin
+echo "${yellow}==>${reset} joplin update..."
+sudo wget -O - https://raw.githubusercontent.com/laurent22/joplin/dev/Joplin_install_and_update.sh | bash 2>&1
+echo "${green}==>${reset} joplin update finished."
 
-# Change shell to Zsh
+# Change shell to Zsh if needed
 chsh -s $(which zsh) $(whoami)
 
-# Setup Tmux plugin manager from scratch (comes with tmux sensible plugin)
+# Setup Tmux plugin manager including "tmux sensible" plugin
 rm -rf ~/.tmux/plugins/
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
-# Grab and install latest release version of LSD (ls replacement). Hacky download command but seems to work well:
+# Grab and install latest release version of LSD (ls replacement). Hacky download command but seems to be durable:
 curl -s -L --output lsd.deb `curl -L https://github.com/Peltoche/lsd/releases/latest -s | grep -E 'lsd_.*_amd64.deb' | sort -u | grep href | cut -d '"' -f2 | sed 's/^/https:\/\/github.com/g'`
-dpkg -i lsd.deb && rm lsd.deb && echo 'alias ls="lsd"' >> ~/.zshrc && echo 'alias la="lsd -al"' >> ~/.zshrc && echo 'alias lt="lsd -l --tree"' >> ~/.zshrc
+sudo apt install ./lsd.deb && rm lsd.deb
 
 # Install bashtop (provides more info than htop)
 git clone https://github.com/aristocratos/bashtop.git /opt/bashtop/
 cd /opt/bashtop/
-make install && echo 'alias top="bashtop"' >> ~/.zshrc && echo 'alias htop="bashtop"' >> ~/.zshrc
+sudo make install
 
-# Disable system beeps
-rmmod pcspkr && echo "blacklist pcspkr" >> /etc/modprobe.d/blacklist.conf
+# Disable annoying system beeps
+sudo rmmod pcspkr
+sudo echo "blacklist pcspkr" >> /etc/modprobe.d/blacklist.conf
 
 # Reminder to refresh env and reboot
 echo "Recommend to reboot now and, once back up, press tmux prefix+I to ensure tmux plugins are initialized."
